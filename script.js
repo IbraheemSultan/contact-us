b84aed34535f24b9b15cf88a9aa193ed481e92ce
Ebn-Sultan <70492711+Ebn-Sultan@users.noreply.github.com>
2025-10-17 21:22:44 +0300

    reduce success message display time to 1 second

--- script.js
+++ script.js
@@ -69,7 +69,7 @@
                 title: "Message Sent Successfully!",
                 text: "We’ll get back to you soon.",
                 showConfirmButton: false,
-                timer: 2000
+                timer: 1000
             });
             contactForm.reset();
         })
